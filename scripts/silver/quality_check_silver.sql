e76a79db8de946f1b4a81dc63bdd9f2dc1a3e10b
Opeyemi Abolade <opeabolade@gmail.com>
2025-03-29 18:14:42 +0000

    Update quality_check_silver.sql

--- scripts/silver/quality_check_silver.sql
+++ scripts/silver/quality_check_silver.sql
@@ -21,3 +21,39 @@ Checking 'silver.crm_cust_info'
 -- ============================================================================
 -- Check for NULLs or Duplicates in Primary Key
 -- Expectation: No Results
+
+SELECT
+cst_id,
+COUNT (*)
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL
+
+
+-- Check for unwanted Spaces
+-- Expectation: No Results
+    
+SELECT 
+	cst_firstname, 
+	cst_lastname
+FROM silver.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname) and 
+	  cst_firstname != TRIM(cst_firstname)
+
+
+-- Data Standardization & Consistency
+    
+SELECT DISTINCT cst_gndr
+FROM silver.crm_cust_info
+
+-- Data Standardization & Consistency
+    
+SELECT DISTINCT cst_marital_status
+FROM silver.crm_cust_info
+
+SELECT * FROM silver.crm_cust_info
+
+-- CLEANING TASKS PERFORMED
+-- 1. Remove Unwanted spaces: Removes unnecesary spaces to ensure data consistency and uniformity across all records.
+-- 2. Data Normalisation & Standardisation: Maps coded values to meaningful, user-friendly descriptions
+-- 3. Ensure only one record per entity by identifying and retaining the most relevant row.
